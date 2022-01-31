61d88702b0ca7280a60bd42b448ee8c5f719378b
Ubuntu <ubuntu@ip-172-26-9-108.eu-west-1.compute.internal>
2022-01-31 02:57:30 +0000

    logistics masters

---
+++ sh_logistics/logistics/doctype/incoterms/incoterms.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, softwarehut and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Incoterms', {
+	// refresh: function(frm) {
+
+	// }
+});
