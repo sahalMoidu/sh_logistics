c00ba7c4f1fb63468d2f2682421f90f4d3684447
Ubuntu <ubuntu@ip-172-26-9-108.eu-west-1.compute.internal>
2022-08-11 09:51:28 +0000

    job p&l

--- sh_logistics/logistics/report/accounts_receivable/accounts_receivable.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2022, softwarehut and contributors
-// For license information, please see license.txt
-/* eslint-disable */
-
-frappe.query_reports["Accounts Receivable"] = {
-	"filters": [
-
-	]
-};
--- sh_logistics/logistics/report/accounts_receivable/accounts_receivable.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, softwarehut and contributors
-# For license information, please see license.txt
-
-# import frappe
-
-def execute(filters=None):
-	columns, data = [], []
-	return columns, data
