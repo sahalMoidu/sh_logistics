3f6cba1e5a2b43db7eca14596354c490cc618449
Ubuntu <ubuntu@ip-172-26-9-108.eu-west-1.compute.internal>
2022-08-17 13:47:10 +0000

    boe

---
+++ sh_logistics/logistics/doctype/bill_of_entry_item/bill_of_entry_item.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, softwarehut and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class BillOfEntryItem(Document):
+	pass
