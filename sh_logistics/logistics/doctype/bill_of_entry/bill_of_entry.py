811e1fac57b2af73d48d1988073f7856781ad2a4
Ubuntu <ubuntu@ip-172-26-9-108.eu-west-1.compute.internal>
2022-08-18 07:08:16 +0000

    boe stock report

--- sh_logistics/logistics/doctype/bill_of_entry/bill_of_entry.py
+++ sh_logistics/logistics/doctype/bill_of_entry/bill_of_entry.py
@@ -2,7 +2,23 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+# from frappe.model.document import Document
 
+# class BillOfEntry(Document):
+# 	pass
+
+from frappe.model.document import Document
+from frappe.model.naming import make_autoname
+import frappe
+from frappe import _
 class BillOfEntry(Document):
-	pass
+	def before_insert(self):
+		if self.stock_movement == "In":
+			for d in self.get("items"):
+				if not d.item_no:
+					d.item_no = make_autoname('BOEI-')
+					# frappe.throw(_(d.item_no))
+		else:
+			for d in self.get("items"):
+				if not d.item_no:
+					frappe.throw(_("Enter Item No"))
